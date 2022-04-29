b5e2bc9e183a21c0ac74a2c94f1180daf6c311a6
enkhjinss <enkhjinnasanbayar7@gmail.com>
2022-04-29 15:49:26 +0800

    created wrapper login component

---
+++ src/assets/FirebaseLogin.js
@@ -0,0 +1,13 @@
+import {
+    GoogleAuthProvider,
+    signInWithPopup,
+    GithubAuthProvider,
+    RecaptchaVerifier,
+    signInWithPhoneNumber,
+} from "firebase/auth";
+import auth from "../firebase";
+//1 user butsaadag bn
+//
+export const FirebaseLogin = () => {
+    return;
+};
